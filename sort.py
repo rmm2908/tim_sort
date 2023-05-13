582cc65c42f9bd0c7df63c05ad99c625476aefcc
Ryan Mitwally <ryan.mitwally@edu.fh-joanneum.at>
2023-05-13 19:56:27 +0200

    added algorithm file

---
+++ sort.py
@@ -0,0 +1,51 @@
+from array import array
+
+
+def sort(a: array, l: int, r: int) -> None:
+    li = l
+    ri = l
+    j = 0
+    for i in range(l, r+1):
+        j += 1
+        if li > ri:
+            ri = li
+        if not (i==r) and (a[i] <= a[i+1]):
+            ri = i+1
+        else:
+            ri = i
+            if j > 1:
+                merge(a, l, ri, li)
+            li = ri+1
+
+
+def merge(a: array, l, r, m):
+    helper = []
+    j = 0
+    ll = l
+    rl = m
+    l_f = False
+    r_f = False
+    for i in range(l, r+1):
+        if ll == m and not l_f:
+            l_f = True
+        if rl > r and not r_f:
+            r_f = True
+        if l_f and r_f:
+            break
+        if l_f:
+            helper.append(a[rl])
+            rl += 1
+        elif r_f:
+            helper.append(a[ll])
+            ll += 1
+        else:
+            if a[ll] <= a[rl]:
+                helper.append(a[ll])
+                ll += 1
+            else:
+                helper.append(a[rl])
+                rl += 1
+
+    for i in range(l, r+1):
+        a[i] = helper[j]
+        j += 1
